20de6a43f2e252b55707dc9ae372618a8fd38ce7
Felina <58801059+felinacodes@users.noreply.github.com>
2026-04-06 20:28:07 +0300

    intro content

--- app/components/notebook/pages/ChapterIntro.tsx
+++ app/components/notebook/pages/ChapterIntro.tsx
@@ -7,7 +7,7 @@ interface ChapterIntroProps {
 
 const ChapterIntro = ({ name, chapterNumber }: ChapterIntroProps) => {
   return (
-    <div className="  h-[20%] w-full mb-2 mt-10">
+    <div className="h-[20%] w-full mb-2 mt-10">
       <div className=" w-full text-center font-jost font-semibold border-b border-b-gray-400 leading-[1.7] text-2xl  ">
         {chapterNumber !== undefined && (
           <p className="text-2xl font-semibold font-serif mb-2">
--- app/components/notebook/pages/Intro.tsx
+++ app/components/notebook/pages/Intro.tsx
@@ -16,19 +16,36 @@ export const IntroBlocks = (args?: RenderContext) => {
         name={"Intro"}
       />
 
-      <div className="relative w-48 h-48 rounded-full overflow-hidden border ">
+      <div className="relative w-48 shrink-0 aspect-square rounded-full overflow-hidden border">
         <Image
           src="/images/my_image.png"
           alt="Portrait"
           fill
-          className="object-cover grayscale"
+          className="object-cover grayscale hover:grayscale-0 transition duration-1000 ease-in "
         />
       </div>
 
-      <div className="flex flex-col gap-2 items-center justify-center mt-4">
-        <h1 className="text-2xl font-semibold">FelinaCodes</h1>
-        <p className="text-sm text-gray-500">Freelance Web Developer</p>
-        <p className="text-sm text-gray-500">React · Next.js · HTML/ CSS/ JS</p>
+      <div className="flex-1 flex flex-col gap-2 items-center justify-center mt-4 p-2 ">
+        <h1 className="text-2xl font-semibold">Maria A.</h1>
+        <p className="whitespace-nowrap text-xl text-gray-500">
+          Freelance Web Developer
+        </p>
+        <div
+          className=" p-2 text-lg text-gray-500 text-center
+          [&>span]:relative 
+          [&>span]:inline-block
+          [&>span]:before:content-['•'] 
+          [&>span]:before:mr-2
+          [&>span]:after:mr-2"
+        >
+          <span>React</span>
+          <span>Next.JS</span>
+          <span>TS</span>
+        </div>
+        <div className="text-lg text-gray-500 flex items-center gap-2 flex-col md:flex-row">
+          <h3>Availability: </h3>
+          <h2>Currently Available </h2>
+        </div>
       </div>
     </section>,
   ];
