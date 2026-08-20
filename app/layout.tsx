7b0ea0c8f3a40fe9bd0857691b6f78488734b9c9
Felina <58801059+felinacodes@users.noreply.github.com>
2026-08-20 18:14:00 +0300

    add vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import { Jost } from "next/font/google";
 import { Merriweather } from "next/font/google";
 import { Providers } from "./providers";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -115,6 +116,7 @@ export default function RootLayout({
       >
         <Providers>{children}</Providers>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
